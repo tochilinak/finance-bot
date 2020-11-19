2babb0e1b30afd6e8b576320a6b82f043d169893
Иван <vasilyevivan.spb@gmail.com>
2020-11-19 16:21:44 +0300

    Bot prototype can reverse text with /reverse command

--- src/bot.py
+++ src/bot.py
@@ -7,8 +7,15 @@ def start_reaction(update: Update, context: CallbackContext):
     update.message.reply_text('lol))')
 
 
+def reverse_reaction(update: Update, context: CallbackContext):
+    text = ' '.join(context.args)
+    reversed_text = text[::-1]
+    context.bot.send_message(chat_id=update.effective_chat.id, text=reversed_text)
+
+
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 dispatcher.add_handler(CommandHandler('start', start_reaction))
+dispatcher.add_handler(CommandHandler('reverse', reverse_reaction))
 updater.start_polling()
