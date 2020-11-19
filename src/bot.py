33d1316376688ce13dc65f1311f1b47ccd7e6449
Иван <vasilyevivan.spb@gmail.com>
2020-11-19 16:21:14 +0300

    Bot prototype reacts to /start message

--- src/bot.py
+++ src/bot.py
@@ -1,1 +1,14 @@
+from src.config import TOKEN
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
+
+def start_reaction(update: Update, context: CallbackContext):
+    update.message.reply_text('lol))')
+
+
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+dispatcher.add_handler(CommandHandler('start', start_reaction))
+updater.start_polling()
