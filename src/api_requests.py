75bb4d39fff744a35655c4a96350fa42bd49a890
Ekaterina Tochilina <katerina_t_n@mail.ru>
2020-11-20 22:23:05 +0300

    moex_cost method added

--- src/api_requests.py
+++ src/api_requests.py
@@ -1,5 +1,13 @@
 import requests
+import json
 
 
 def moex_cost(symbol):
-    pass
+    query = ("https://iss.moex.com/iss/engines/stock/markets/shares"
+             "/boards/TQBR/securities.json?securities.columns=SECID,"
+             "PREVADMITTEDQUOTE&iss.meta=off&iss.only=securities")
+    resp_raw = requests.get(query).text
+    resp = json.JSONDecoder().decode(resp_raw)
+    result = next((x[1] for x in resp['securities']['data']
+                  if x[0] == symbol), None)
+    return result
