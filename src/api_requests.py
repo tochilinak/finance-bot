e26c2e90682ac56e4be2d2614259410cee241856
Ekaterina Tochilina <katerina_t_n@mail.ru>
2020-11-20 23:24:11 +0300

    Code simplified

--- src/api_requests.py
+++ src/api_requests.py
@@ -7,8 +7,7 @@ def moex_cost(symbol):
     query = ("https://iss.moex.com/iss/engines/stock/markets/shares"
              "/boards/TQBR/securities.json?securities.columns=SECID,"
              "PREVADMITTEDQUOTE&iss.meta=off&iss.only=securities")
-    resp_raw = requests.get(query).text
-    resp = json.JSONDecoder().decode(resp_raw)
+    resp = requests.get(query).json()
     result = next((x[1] for x in resp['securities']['data']
                   if x[0] == symbol), None)
     return result
@@ -25,8 +24,7 @@ def marketstack_cost(symbol):
 def moex_symbol_by_name(name):
     query = ("https://iss.moex.com/iss/securities.json?q=" + name
              + "&securities.columns=name,secid,group&iss.meta=off")
-    resp_raw = requests.get(query).text
-    resp = json.JSONDecoder().decode(resp_raw)
+    resp = requests.get(query).json()
     only_stock = filter(lambda x: x[2] == "stock_shares" and
                         x[1].isupper(), resp['securities']['data'])
     return [x[:2] for x in only_stock]
