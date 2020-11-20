c82e97f4d3dea927c463c4730414a64ba0aea036
Maria <radmarale@gmail.com>
2020-11-20 23:07:45 +0300

    Added marketstack_cost method.

--- src/api_requests.py
+++ src/api_requests.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import config
 
 
 def moex_cost(symbol):
@@ -10,4 +11,11 @@ def moex_cost(symbol):
     resp = json.JSONDecoder().decode(resp_raw)
     result = next((x[1] for x in resp['securities']['data']
                   if x[0] == symbol), None)
+    return result
+
+def marketstack_cost(symbol):
+    query = "http://api.marketstack.com/v1/tickers/" + symbol + "/intraday/latest"
+    params = {"access_key": config.API_KEY_MARKETSTACK}
+    r = requests.get(query, params=params)
+    result = r.json()["close"]
     return result
