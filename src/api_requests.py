a003b8bb0b229bc4fd0f423cde7cd9d4f5dc7db7
Ekaterina Tochilina <katerina_t_n@mail.ru>
2020-11-21 23:38:57 +0300

    Some docstrings added; moex_symbol_by_name improved

--- src/api_requests.py
+++ src/api_requests.py
@@ -13,6 +13,14 @@ def moex_cost(symbol):
     return result
 
 
+def moex_company_list():
+    query = ("https://iss.moex.com/iss/engines/stock/markets/shares/"
+             "boards/TQBR/securities.json?securities.columns=SECID&"
+             "iss.meta=off&iss.only=securities")
+    resp = requests.get(query).json()
+    return [x[0] for x in resp['securities']['data']]
+
+
 def marketstack_cost(symbol):
     query = "http://api.marketstack.com/v1/tickers/" + symbol +\
             "/intraday/latest"
@@ -24,10 +32,11 @@ def marketstack_cost(symbol):
 
 def moex_symbol_by_name(name):
     query = ("https://iss.moex.com/iss/securities.json?q=" + name
-             + "&securities.columns=name,secid,group&iss.meta=off")
+             + "&securities.columns=name,secid&iss.meta=off")
     resp = requests.get(query).json()
-    only_stock = filter(lambda x: x[2] == "stock_shares" and
-                        x[1].isupper(), resp['securities']['data'])
+    company_list = moex_company_list()
+    only_stock = filter(lambda x: x[1] in company_list,
+                        resp['securities']['data'])
     return [x[:2] for x in only_stock]
 
 
@@ -41,14 +50,23 @@ def alphavantage_symbol_by_name(name):
 
 
 def current_cost(symbol):
+    """Find stock price by symbol.
+
+    Returns None if not found
+    """
     marketstack_result = marketstack_cost(symbol)
     if marketstack_result is not None:
         return marketstack_result
     return moex_cost(symbol)
 
 
-def symbol_by_name(name):
-    result_size = 5
+def symbol_by_name(name, result_size=5):
+    """Find symbol by company name using American and Russian api's.
+
+    Returns list of pairs [full_name, symbol].
+    result_size (default: 5) - maximum number of companies
+    returned by one api
+    """
     moex_result = moex_symbol_by_name(name)[:result_size]
     alphavantage_result = alphavantage_symbol_by_name(name)[:result_size]
     return moex_result + alphavantage_result
