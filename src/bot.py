3b682c071a519aa5819a2cbc354d6345487b2e83
Иван <vasilyevivan.spb@gmail.com>
2020-11-21 21:34:12 +0300

    Edited conversation

--- src/bot.py
+++ src/bot.py
@@ -19,57 +19,11 @@ def help_bot(update: Update, context: CallbackContext):
     )
 
 
-def company_choise_handler(fallbacks, action_after_select, next_key, exchange_selection=True):
-    """
-    Note: action_after_select must return next_key
-    """
-    def stock_exchange(update: Update, context: CallbackContext):
-        text = update.message.text
-        context.user_data['stock_exchange'] = text
-        update.message.reply_text("Ok, now enter a company ticker")
-        return "company"
-
-    def skip_stock_exchange(update: Update, context: CallbackContext):
-        update.message.reply_text("Ok, just enter a company ticker")
-        return "company"
-
-    def company(update: Update, context: CallbackContext):
-        text = update.message.text
-        context.user_data['company'] = text
-        next_key_from_action = action_after_select(update, context)
-
-        try:
-            assert next_key_from_action == next_key
-        except AssertionError:
-            print("next_key_from_action != next_key")
-            update.message.reply_text("Something went wrong")
-            return ConversationHandler.END
-
-        return next_key
-
-    if exchange_selection:
-        handler = ConversationHandler(
-            entry_points=[CommandHandler("skip", skip_stock_exchange),
-                          MessageHandler(Filters.text & ~Filters.command, stock_exchange)],
-            states={
-                "company": [MessageHandler(Filters.text & ~Filters.command, company)]
-            },
-            fallbacks=fallbacks,
-            map_to_parent={
-                next_key: next_key
-            }
-        )
-    else:
-        handler = MessageHandler(Filters.text & ~Filters.command, company)
-
-    return handler
-
-
 def price_start(update: Update, context: CallbackContext):
     update.message.reply_text("Ok, now I need to know the company you are interested in\n" "Enter stock_exchange name "
                               "or /skip")
     update.message.reply_text("You can stop with /cancel")
-    return "stock_exchange"
+    return "company"
 
 
 def cancel(update: Update, context: CallbackContext):
@@ -77,12 +31,14 @@ def cancel(update: Update, context: CallbackContext):
     return ConversationHandler.END
 
 
-def request_period(update: Update, context: CallbackContext):
+def get_company(update: Update, context: CallbackContext):
+    text = update.message.text
+    context.user_data['company'] = text
     update.message.reply_text("Enter a period")
     return "period"
 
 
-def give_price(update: Update, context: CallbackContext):
+def get_period(update: Update, context: CallbackContext):
     text = update.message.text
     context.user_data['period'] = text
     update.message.reply_text("Price of " + context.user_data['company'] + " is unknown :(")
@@ -100,8 +56,8 @@ def main():
         ConversationHandler(
             entry_points=[CommandHandler("price", price_start)],
             states={
-                "stock_exchange": [company_choise_handler([cancel_handler], request_period, "period")],
-                "period": [MessageHandler(Filters.text & ~Filters.command, give_price)]
+                "company": [MessageHandler(Filters.text & ~Filters.command, get_company)],
+                "period": [MessageHandler(Filters.text & ~Filters.command, get_period)]
             },
             fallbacks=[cancel_handler]
         )
