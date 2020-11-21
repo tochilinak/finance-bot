74bb1154c21a5cc86191ede0883952c490fa578a
Maria <radmarale@gmail.com>
2020-11-21 17:00:17 +0300

    Did some changes with code with pep8 rules.

--- src/api_requests.py
+++ src/api_requests.py
@@ -14,7 +14,8 @@ def moex_cost(symbol):
 
 
 def marketstack_cost(symbol):
-    query = "http://api.marketstack.com/v1/tickers/" + symbol + "/intraday/latest"
+    query = "http://api.marketstack.com/v1/tickers/" + symbol +\
+            "/intraday/latest"
     params = {"access_key": config.API_KEY_MARKETSTACK}
     r = requests.get(query, params=params).json()
     result = r["close"] if "close" in r.keys() else None
@@ -32,7 +33,8 @@ def moex_symbol_by_name(name):
 
 def alphavantage_symbol_by_name(name):
     query = "https://www.alphavantage.co/query"
-    params = {"function": "SYMBOL_SEARCH", "keywords": name, "apikey": config.API_KEY_ALPHAVANTAGE}
+    params = {"function": "SYMBOL_SEARCH", "keywords": name, "apikey":
+              config.API_KEY_ALPHAVANTAGE}
     r = requests.get(query, params=params).json()["bestMatches"]
     result = r[0]["1. symbol"] if len(r) > 0 else None
     return result
