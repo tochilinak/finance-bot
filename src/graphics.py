57de617ce9b90a875fc9dec292ac525d2621b634
Ekaterina Tochilina <katerina_t_n@mail.ru>
2020-11-25 19:54:33 +0300

    Dates methods instead of general ones

--- src/graphics.py
+++ src/graphics.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
 
 
-def draw_plot(x_axis, y_axis, image_filename):
+def draw_plot(datetime_values, y_values, image_filename):
     """
     Draw plot and save into image_filename.svg
-    
-    x_axis and y_axis are lists.
-    Each node (x, y) is represented by
-    (x_axis[i], y_axis[i])
+
+    datetime_values is a list of datetime objects,
+    y_values is a list of int.
+    Each node's coordinates (x, y) are:
+    (datetime_values[i], y_values[i])
     """
     fig, ax = plt.subplots()
-    ax.plot(x_axis, y_axis)
+    ax.plot(datetime_values, y_values)
+    fig.autofmt_xdate()
     plt.savefig(image_filename + ".svg", format="svg")
