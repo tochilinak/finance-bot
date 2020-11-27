d550f5d8b50f84f19d1c6828e5fa6a437baac0bc
Иван <vasilyevivan.spb@gmail.com>
2020-11-27 11:17:45 +0300

    Started adding the ability to process periods of different types for /price

--- src/bot.py
+++ src/bot.py
@@ -42,54 +42,79 @@ def cancel(update: Update, context: CallbackContext):
 
 def price_start(update: Update, context: CallbackContext):
     """
-    Ask user about company ticker. This callback is executed by /price.
+    This callback is executed by /price.
 
-    Return key of next part of conversation
+    Give the price of the company's shares for the specified period in the end
+    of conversation.
+
+    Return key of the next part of conversation.
     """
+    # context.args is list of words after command
+    if not context.args:
+        update.message.reply_text(
+            'You can use the command like this: "/price <company ticker>"'
+        )
+        update.message.reply_text(
+            "Ok, now I need to know the company you are interested in\n"
+            "Enter a company ticker "
+        )
+        return "ticker"
+
+    ticker = ' '.join(context.args)
+    context.user_data["ticker"] = ticker
+
     update.message.reply_text(
-        "Ok, now I need to know the company you are interested in\n"
-        "Enter a company ticker "
+        "For what period are you interested in the price?"
     )
-    update.message.reply_text("You can stop with /cancel")
-    return "company"
-
-
-def get_company(update: Update, context: CallbackContext):
-    """
-    Get company ticker from user and start task.
-
-    This task will tell the user company stock price.
-    In the end, stop the conversation
-    """
-    text = update.message.text
-    context.job_queue.run_once(
-        callback=give_price,
-        when=0,
-        context={'chat_id': update.message.chat.id, "company": text}
+    update.message.reply_text(
+        "You can get information about entering "
+        "a period with the / periods command"
     )
-    return ConversationHandler.END
+    return "period_type"
 
 
-def give_price(context: CallbackContext):
-    """Send the company price to the chat with specified chat id."""
-    # get information about chat id and company
-    job_context: Optional[dict] = context.job.context
-    company = job_context["company"]
-    chat_id = job_context["chat_id"]
+def get_ticker(update: Update, context: CallbackContext):
+    """Get company ticker from user and ask about period."""
+    ticker = update.message.text
+    context.user_data["ticker"] = ticker
 
-    ticker = company.upper()
+    update.message.reply_text(
+        "For what period are you interested in the price?"
+    )
+    update.message.reply_text(
+        "You can get information about entering "
+        "a period with the /periods command"
+    )
+    return "period_type"
+
+
+def periods(update: Update, context: CallbackContext):
+    """Give user information about entering a period."""
+    update.message.reply_text(
+        "/last_update - get the most current price available\n"
+    )
+    return "period_type"
+
+
+def current_price(update: Update, context: CallbackContext):
+    """Send the company current price to the chat with specified chat id."""
+    ticker = context.user_data["ticker"]
+
+    ticker = ticker.upper()
 
     price = current_cost(ticker)
 
     if price is not None:
-        message_text = company + " stock price is " + str(price)
+        message_text = ticker + " stock price is " + str(price)
     else:
         message_text = "I don't know this price"
 
     context.bot.send_message(
-        chat_id=chat_id,
+        chat_id=update.message.chat_id,
         text=message_text
     )
+
+    return ConversationHandler.END
 
 
 def find_company(update: Update, context: CallbackContext):
@@ -104,7 +129,7 @@ def find_company(update: Update, context: CallbackContext):
     # context.args is list of words after command
     if not context.args:
         update.message.reply_text(
-            'You can use the command like this: "/find_company <company name>'
+            'You can use the command like this: "/find_company <company name>"'
         )
         update.message.reply_text(
             "Ok, now I need to know the company you are interested in\n"
@@ -192,12 +217,19 @@ def main():
     # Create handler for /cancel command
     cancel_handler = CommandHandler("cancel", cancel)
 
+    # Create handlers for different period types
+    period_type_hanlers = [
+        CommandHandler("last_update", current_price),
+        CommandHandler("periods", periods)
+    ]
+
     # Add conversation handler for /price command
     dispatcher.add_handler(
         ConversationHandler(
             entry_points=[CommandHandler("price", price_start)],
             states={
-                "company": [MessageHandler(simple_text_filter, get_company)],
+                "ticker": [MessageHandler(simple_text_filter, get_ticker)],
+                "period_type": period_type_hanlers
             },
             fallbacks=[cancel_handler]
         )
