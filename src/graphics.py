ebddc1ef82a7f8758c496fef93e17ecf8dd622df
Ekaterina Tochilina <katerina_t_n@mail.ru>
2020-11-28 19:27:08 +0300

    Label formatters changed (now they are different for small and big data)

--- src/graphics.py
+++ src/graphics.py
@@ -11,6 +11,31 @@ dates, values = get_period_data_of_cost("2015-10-01",
 draw_plot(dates, values, "out")
 """
 
+def set_small_data_labels(datetime_values, ax):
+    ax.set_xticks(datetime_values)
+    # check whether datetimes represent dates or time
+    if all(x.hour == 0 and x.minute == 0 for x in datetime_values):
+        labels = [x.strftime('%Y-%m-%d') for x in datetime_values]
+    else:
+        labels = [x.strftime('%H:%M') for x in datetime_values]
+    ax.set_xticklabels(labels)
+
+
+def set_big_data_labels(datetime_values, ax):
+    locator = AutoDateLocator()
+    formatter = AutoDateFormatter(locator)
+    formatter.scaled = my_scaled
+    ax.xaxis.set_major_locator(locator)
+    ax.xaxis.set_major_formatter(formatter)
+
+
+def set_labels(datetime_values, ax):
+    if len(datetime_values) < 7:
+        set_small_data_labels(datetime_values, ax)
+    else:
+        set_big_data_labels(datetime_values, ax)
+
+
 my_scaled = {
     365.0: '%Y',
     30.0: '%Y-%m',
@@ -30,13 +55,10 @@ def draw_plot(datetime_values, y_values, image_filename):
     """
     fig, ax = plt.subplots()
     ax.plot(datetime_values, y_values)
+    # add big dots if data is small
     if len(datetime_values) <= 30:
         ax.plot_date(datetime_values, y_values)
-    locator = AutoDateLocator()
-    formatter = AutoDateFormatter(locator)
-    formatter.scaled = my_scaled
-    ax.xaxis.set_major_locator(locator)
-    ax.xaxis.set_major_formatter(formatter)
+    set_labels(datetime_values, ax)
     fig.autofmt_xdate()
 
     plt.savefig(image_filename + ".svg", format="svg")
