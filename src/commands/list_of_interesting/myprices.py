10d6de1e96985ce418268f3047accf717be8fea8
Иван <vasilyevivan.spb@gmail.com>
2020-12-06 14:41:04 +0300

    /myprices now gives list of companies and their prices (not only list of companies)

--- src/commands/list_of_interesting/myprices.py
+++ src/commands/list_of_interesting/myprices.py
@@ -1,15 +1,27 @@
-from telegram import Update
+from telegram import Update, ParseMode
 from telegram.ext import (
     CommandHandler,
     CallbackContext
 )
 from database import list_users_tickers
+from api_requests import current_cost
+
+
+def info_line(ticker: str):
+    price = str(current_cost(ticker))
+    price = price.replace(".", r"\.")
+    return "*%s:* %s" % (ticker, price)
 
 
 def myprices(update: Update, context: CallbackContext):
     chat_id = update.message.chat_id
-    text = list_users_tickers(chat_id)
-    update.message.reply_text(text)
+    tickers = list_users_tickers(chat_id)
+    text = "\n".join([info_line(ticker) for ticker in tickers])
+    context.bot.send_message(
+        text=text,
+        chat_id=chat_id,
+        parse_mode=ParseMode.MARKDOWN_V2
+    )
 
 
 myprices_handler = CommandHandler("myprices", myprices)
