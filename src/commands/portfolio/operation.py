7c07e72025dc6129f6b8ac0a3be50952dfd0f20d
Иван <vasilyevivan.spb@gmail.com>
2020-12-17 16:47:48 +0300

    Added operation handlers (with no actions)

---
+++ src/commands/portfolio/operation.py
@@ -0,0 +1,29 @@
+from telegram import Update
+from telegram.ext import (
+    CommandHandler,
+    CallbackContext
+)
+from re import match
+from commands.bot_filters import operation_info
+
+
+def handle_operation(update: Update, context: CallbackContext):
+    text: str = " ".join(context.args)
+    if not match(operation_info, text):
+        update.message.reply_text(
+            "Enter information about the operation after command\n"
+            "Format: ticker, number, date(YYYY-MM-DD)\n"
+            "Example: GOOGL, 7, 20017-10-13\n"
+        )
+        return
+
+    ticker, number, date = text.split(", ")
+    if match(r'^/buy*', update.message.text):
+        pass
+    else:
+        pass
+    pass
+
+
+buy_handler = CommandHandler("buy", handle_operation)
+sell_handler = CommandHandler("sell", handle_operation)
