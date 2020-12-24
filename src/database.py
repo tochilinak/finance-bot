e7ee007d499f09a12693f7c67223f42b4af20170
Maria <radmarale@gmail.com>
2020-12-24 23:00:05 +0300

    Add price parametr to add_operation method.

--- src/database.py
+++ src/database.py
@@ -1,3 +1,4 @@
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
@@ -95,20 +96,9 @@ def delete_users_ticker(telegram_address: int, symbol: str):
     session.commit()
 
 
-def add_operation(telegram_address: int, symbol: str, count: int, date: str, operation_type: IntEnum):
-    """Add operation to the table.
-
-    Types of parameters:
-    :param telegram_address: Integer;
-    :param symbol: String;
-    :param count: Integer;
-    :param date: String;
-    :param operation_type: enum operation_type object.
-    """
-    if len(get_period_data_of_cost(date, date, symbol)[1]) == 0:
-        # Date isn't a trade day.
-        return False
-    price = get_period_data_of_cost(date, date, symbol)[1][0]
+def add_operation(telegram_address: int, symbol: str, count: int,
+                  price: float, date: str, operation_type: IntEnum):
+    """Add operation to the table."""
     currency = get_currency(symbol)
     current_operation = Operations(telegram_address=telegram_address,
                                    company_symbol=symbol,
@@ -159,7 +149,8 @@ def get_list_of_operations(telegram_address: int):
     session.commit()
 
 
-def get_prefix_balance(user_data: sqlalchemy.orm.query.Query, end_date="9999-99-99"):
+def get_prefix_balance(user_data: sqlalchemy.orm.query.Query,
+                       end_date="9999-99-99"):
     """Get user's balance by a period contained in user_data.
 
     :param user_data: SQLAlchemy query object.
@@ -180,7 +171,8 @@ def get_prefix_balance(user_data: sqlalchemy.orm.query.Query, end_date="9999-99-
     return balance
 
 
-def get_prefix_count_of_stocks(user_data: sqlalchemy.orm.query.Query, end_date="9999-99-99"):
+def get_prefix_count_of_stocks(user_data: sqlalchemy.orm.query.Query,
+                               end_date="9999-99-99"):
     """Get count of stocks from every company from a\
     period contained in user_data.
 
